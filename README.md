6d1eeef0e09fe09a2ae484434fa8beba0bd2fcd8
Thomas Janssen <thomas@rubix.nl>
2021-02-06 11:02:14 +0100

    Added model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # p1mon-vaadin-ui
+ 
+- http://www.p1mon.nl/api/v1/smartmeter/help
+- http://www.p1mon.nl/api/v1/smartmeter?limit=1
+
+![](images/Mockup.png)
+
--- src/main/java/nl/degup/spring/p1monui/model/P1monResponse.java
+++ src/main/java/nl/degup/spring/p1monui/model/P1monResponse.java
@@ -8,6 +8,34 @@ import java.util.Date;
 public class P1monResponse {
     private final Date timestamp;
     private final String utcTimestamp;
+    private final boolean isProcessed; // 0 / 1 in api
+    private final long consumptionKwhLowTotal;
+    private final long consumptionKwhHighTotal;
+    private final long productionKwhLowTotal;
+    private final long productionKwhHighTotal;
+    private String tarifCode; // Low = D, High = P
+    private final int consumptionWLow;
+    private final int consumptionWHigh;
+    private final int productionWLow;
+    private final int productionWHigh;
+    private final long consumptionGarM3;
 
+/*
+[
+  [
+    "2021-02-06 10:13:42",
+    1612602822,
+    0,
+    6570.936,
+    5251.012,
+    37.539,
+    64.36,
+    "D",
+    121,
+    0,
+    4128.063
+  ]
+]
+ */
 
 }
